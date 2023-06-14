a3b6fd703ec59caf5576ac9cad26528b1ad13dee
Jon Higgins <jon@jonhiggins.co.uk>
2023-06-14 18:53:34 +0100

    Add menu for scenes

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Leap.Unity.Interaction;
 
 public class SceneChanger : MonoBehaviour
 {
+    private string currentScene = "0 Home";
+
     public void ChangeScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        if (currentScene != "0 Home") 
+        {
+            SceneManager.UnloadScene(currentScene);
+        }
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        currentScene = sceneName;
     }
 }
