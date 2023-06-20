094c92713dd985e6509c567b67b418ed47f53cdf
Jon Higgins <jon@jonhiggins.co.uk>
2023-06-20 15:31:51 +0100

    Add controller trigger method of skipping scenes

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -6,15 +6,60 @@ using Leap.Unity.Interaction;
 
 public class SceneChanger : MonoBehaviour
 {
-    private string currentScene = "0 Home";
+    private int currentSceneIndex = -1;
+    public string[] scenes;
+    private List<UnityEngine.XR.InputDevice> devices;
+    bool changingScenes = false;
 
-    public void ChangeScene(string sceneName)
+    private void Start()
     {
-        if (currentScene != "0 Home") 
+        devices = new List<UnityEngine.XR.InputDevice>();
+    }
+
+
+    private void Update()
+    {
+        if (devices.Count == 0)
         {
-            SceneManager.UnloadScene(currentScene);
+            InitialiseXrDevice();
+            return;
         }
-        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
-        currentScene = sceneName;
+        devices.ForEach((device) =>
+        {
+            bool triggerValue;
+            if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue) && triggerValue)
+            {
+                ChangeScene(currentSceneIndex + 1);
+            }
+        });
+
+
+    }
+
+    private void InitialiseXrDevice()
+    {
+        UnityEngine.XR.InputDevices.GetDevices(devices);
+    }
+
+    public void ChangeScene(int newSceneIndex)
+    {
+        if (changingScenes || newSceneIndex >= scenes.Length)
+        {
+            return;
+        }
+        changingScenes = true;
+        if (currentSceneIndex != -1)
+        {
+            SceneManager.UnloadSceneAsync(scenes[currentSceneIndex]);
+        }
+        SceneManager.LoadScene(scenes[newSceneIndex], LoadSceneMode.Additive);
+        currentSceneIndex = newSceneIndex;
+        StartCoroutine(DebounceSceneChange());
+    }
+
+    private IEnumerator DebounceSceneChange()
+    {
+        yield return new WaitForSeconds(0.3f);
+        changingScenes = false;
     }
 }
