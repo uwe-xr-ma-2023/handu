692035dfe5934fbc783ef1e60a1f76d7fbab68cc
Jon Higgins <jon@jonhiggins.co.uk>
2023-06-21 16:38:38 +0100

    Add audio to 4.2 particle play

---
+++ Assets/Scripts/4.2 Particle Play/ParticlePlaySound.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlePlaySound : MonoBehaviour
+{
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    public void PlayAudio()
+    {
+        audioSource.Play();
+    }
+
+    public void StopAudio()
+    {
+        audioSource.Stop();
+    }
+}
